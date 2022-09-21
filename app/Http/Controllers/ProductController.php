c9808ddb3f386758b1468a0278982e8f7534489c
Rafael <rafael@Rafaels-MacBook-Air.local>
2022-09-21 02:39:07 -0300

    Added controller and route to products

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Repositories\Interfaces\ProductRepositoryInterface;
+
+class ProductController extends Controller
+{
+    private $productRepository;
+
+    public function __construct(ProductRepositoryInterface $productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
+
+    public function index()
+    {
+        $products = $this->productRepository->all();
+
+        return response()->json($products);
+    }
+}
