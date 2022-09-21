dc53e6419a431b1c7c0fdb95b49a496757fdea24
Rafael <rafael@Rafaels-MacBook-Air.local>
2022-09-21 02:38:35 -0300

    Added product repository and interface

---
+++ app/Repositories/ProductRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Product;
+use App\Repositories\Interfaces\ProductRepositoryInterface;
+
+class ProductRepository implements ProductRepositoryInterface
+{
+    public function all()
+    {
+        return Product::all();
+    }
+}
