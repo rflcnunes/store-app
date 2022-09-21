a24d405a06b445a40c75835e518ff80c00ab14e0
Rafael <rafael@Rafaels-MacBook-Air.local>
2022-09-21 02:36:19 -0300

    Added Product Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            ProductSeeder::class,
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        echo "Database seeded!" . PHP_EOL;
     }
 }
